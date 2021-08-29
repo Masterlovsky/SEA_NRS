ee7975f90bc3f9f18ca7e0eea1ee64b025fa5dea
Masterlovsky <masterlovsky@gmail.com>
2021-08-29 17:09:51 +0800

    发包脚本修复报文格式

--- src/main/java/nnnmc/seanet/seanrs/test/sendRawPacket.py
+++ src/main/java/nnnmc/seanet/seanrs/test/sendRawPacket.py
@@ -11,8 +11,8 @@ class IDP(Packet):
 
                    ByteField("EID_Type", 0),
                    ShortField("reserved", 1),
-                   StrField("sourceEID", '0' * 40),
-                   StrField("destEID", '0' * 40),
+                   XNBytesField("sourceEID", int("0" * 40, 16), 20),
+                   XNBytesField("destEID", int("a" * 40, 16), 20),
                    ]
 
 
@@ -24,18 +24,26 @@ class IDPNRS(Packet):
                                                      0x04: "deregister_resp", 0x05: "resolve", 0x06: "resolve_w"}),
                    ByteField("BGPType", 0),
                    ByteEnumField("source", 0, {0: "format1", 1: "format2"}),
-                   StrField("na", '0' * 32),
+                   XNBytesField("na", int("0" * 32, 16), 16),
                    ]
 
 
 def main():
-    payload = "hello World"
-    pkt = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="::0", dst="2400:dd01:1037:201:192:168:47:191") / IDP(
-        destEID="9" * 40) / IDPNRS(queryType="resolve") / payload
-    # pkt = Ether() / IPv6(dst="2400:dd01:1037:201:192:168:47:198")
+    payload_register = "6f" + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" + "66666666666666666666666666666666"
+    payload_deregister = "73" + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" + "66666666666666666666666666666666"
+    pkt_r = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="2400:dd01:1037:9:192:168:9:201", dst="::") / IDP(
+        destEID=int("0" * 40, 16)) / IDPNRS(queryType="register") / bytes.fromhex(payload_register)
+    pkt_d = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="2400:dd01:1037:9:192:168:9:201", dst="::") / IDP(
+        destEID=int("0" * 40, 16)) / IDPNRS(queryType="register") / bytes.fromhex(payload_deregister)
+    pkt_eq = Ether(dst="A4:23:05:00:11:02") / IPv6(nh=0x99, src="2400:dd01:1037:9:192:168:9:201",
+                                                   dst="2400:dd01:1037:9:192:168:9:201") / IDP(
+        destEID=int("b" * 40, 16)) / IDPNRS(queryType="resolve")
+    # pkt_r = Ether() / IPv6(dst="2400:dd01:1037:201:192:168:47:198")
+    pkt = pkt_eq
     pkt.show()
-    # sendp(pkt, loop=1, inter=0.2)
-    sendp(pkt, count=1)
+    hexdump(pkt)
+    # sendp(pkt, iface="em4", loop=1, inter=0.2)
+    # sendp(pkt, iface="em4", count=1)
 
 
 if __name__ == '__main__':
